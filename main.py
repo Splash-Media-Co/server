53e96734d38d73de5458aaed5cee15862bc5a1be
notfenixio <103071021+NotFenixio@users.noreply.github.com>
2023-12-09 18:42:06 +0000

    Pushing from the gitpod srv to repo

---
+++ main.py
@@ -0,0 +1,20 @@
+# Import the server
+from cloudlink import server
+
+# Import protocols
+from cloudlink.server.protocols import clpv4, scratch
+
+# Instantiate the server object
+server = server()
+
+# Set logging level
+server.logging.basicConfig(
+    level=server.logging.DEBUG # See python's logging library for details on logging levels.
+)
+
+# Load protocols
+clpv4 = clpv4(server)
+scratch = scratch(server)
+
+# Start the server!
+server.run(ip="127.0.0.1", port=3000)
