f903da477c32a6a89e2fb197bac53c0244a11469
GitHub <noreply@github.com>
2023-12-10 12:59:17 +0000

    Run Ruff Linter & Formatter
    Signed-off-by: GitHub <noreply@github.com>

--- owodb.py
+++ owodb.py
@@ -1,10 +1,12 @@
 # owodb.py
 import json
 import os
-from logs import Info, Warning, Debug, Error, Critical
+from logs import Info, Warning, Error
+
 
 class OwODB:
-    """"Your average homie that identifies as a JSON DB."""
+    """ "Your average homie that identifies as a JSON DB."""
+
     def __init__(self, db_name):
         self.db_name = db_name
         self.db_path = f"{db_name}.json"
@@ -13,16 +15,16 @@ class OwODB:
 
     def load_data(self):
         if os.path.exists(self.db_path):
-            with open(self.db_path, 'r') as file:
+            with open(self.db_path, "r") as file:
                 self.data = json.load(file)
 
     def save_data(self):
-        with open(self.db_path, 'w') as file:
+        with open(self.db_path, "w") as file:
             json.dump(self.data, file, indent=2)
 
     def create_table(self, table_name, columns):
         if table_name not in self.data:
-            self.data[table_name] = {'columns': columns, 'data': []}
+            self.data[table_name] = {"columns": columns, "data": []}
             self.save_data()
             Info(f"Table '{table_name}' created successfully.")
         else:
@@ -30,9 +32,9 @@ class OwODB:
 
     def insert_data(self, table_name, values):
         if table_name in self.data:
-            if len(values) == len(self.data[table_name]['columns']):
-                row = dict(zip(self.data[table_name]['columns'], values))
-                self.data[table_name]['data'].append(row)
+            if len(values) == len(self.data[table_name]["columns"]):
+                row = dict(zip(self.data[table_name]["columns"], values))
+                self.data[table_name]["data"].append(row)
                 self.save_data()
                 Info("Data inserted successfully.")
             else:
@@ -42,9 +44,13 @@ class OwODB:
 
     def select_data(self, table_name, conditions=None):
         if table_name in self.data:
-            result = self.data[table_name]['data']
+            result = self.data[table_name]["data"]
             if conditions:
-                result = [row for row in result if all(row.get(col) == value for col, value in conditions.items())]
+                result = [
+                    row
+                    for row in result
+                    if all(row.get(col) == value for col, value in conditions.items())
+                ]
             return result
         else:
             Warning(f"Table '{table_name}' does not exist.")
@@ -52,8 +58,10 @@ class OwODB:
 
     def update_data(self, table_name, set_values, conditions=None):
         if table_name in self.data:
-            for row in self.data[table_name]['data']:
-                if conditions and not all(row.get(col) == value for col, value in conditions.items()):
+            for row in self.data[table_name]["data"]:
+                if conditions and not all(
+                    row.get(col) == value for col, value in conditions.items()
+                ):
                     continue
                 for col, value in set_values.items():
                     if col in row:
@@ -66,10 +74,15 @@ class OwODB:
     def delete_data(self, table_name, conditions=None):
         if table_name in self.data:
             if conditions:
-                self.data[table_name]['data'] = [row for row in self.data[table_name]['data']
-                                                 if not all(row.get(col) == value for col, value in conditions.items())]
+                self.data[table_name]["data"] = [
+                    row
+                    for row in self.data[table_name]["data"]
+                    if not all(
+                        row.get(col) == value for col, value in conditions.items()
+                    )
+                ]
             else:
-                self.data[table_name]['data'] = []
+                self.data[table_name]["data"] = []
             self.save_data()
             Info("Data deleted successfully.")
         else:
