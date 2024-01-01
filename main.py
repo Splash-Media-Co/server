1398deb7b52933090ed755a8f6643a37806bcdfb
notfenixio <josueart40@gmail.com>
2024-01-01 19:12:39 +0000

    auth implementation (VERY WIP)

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # Import the server
 from cloudlink import server
 
+# Import JSON Web token handler
+import jwt  # noqa: F401
+
+# Import cryptography
+from cryptography.fernet import Fernet  # noqa: F401
+
 # Import logging helpers
 from logs import Info, Warning, Debug, Error, Critical  # noqa: F401
 
@@ -17,9 +23,10 @@ import uuid
 # Import protocols
 from cloudlink.server.protocols import clpv4, scratch
 
-# Import signal handling and sys
+# Import signal handling, sys, and os
 import signal
 import sys
+import os
 
 # Instantiate the server object
 server = server()
@@ -36,10 +43,20 @@ server.logging.basicConfig(
 clpv4 = clpv4(server)
 scratch = scratch(server)
 
+# Load secret
+KEY = os.getenv('KEY')
+
+authenticated_clients = []
 
 @server.on_connect
 async def on_connect(client):
-    Info(f"Client {str(client)} connected")
+    Info(f"Client {str(client.id)} connected")
+
+@server.on_disconnect
+async def on_disconnect(client):
+    Info(f"Client {str(client.id)} disconnected")
+    if client.id in authenticated_clients:
+        authenticated_clients.remove(client.id)
 
 
 """
@@ -63,55 +80,145 @@ async def direct(client, message):
         case "post":
             match str(message["val"]["val"]["type"]):
                 case "send":
-                    Info(
-                        f"Client {str(client.id)} sent message: Post: {str(message["val"]["val"]["p"])}, mode: {str(message["val"]["val"]["type"])}, timestamp: {float(time.time())}"
-                    )
-                    uid = str(uuid.uuid4())
-                    db.insert_data(
-                        "posts",
-                        (
-                            str(client.username),
-                            float(time.time()),
-                            uid,
-                            str(message["val"]["val"]["p"]),
-                            False,
-                            "home",
-                            str(message["val"]["val"]["type"]),
-                        ),
-                    )
-                    server.send_packet_multicast(
-                        server.clients_manager.clients,
-                        {
-                            "cmd": "gmsg",
-                            "val": {
-                                "cmd": "rpost",
+                    if (client.id not in authenticated_clients):
+                        try:
+                            await server.send_packet_unicast(
+                                client,
+                                {
+                                    "cmd": "gmsg",
+                                    "val": {
+                                        "cmd": "status",
+                                        "val": {
+                                            "message": "Not authenticated",
+                                            "username": client.username
+                                        }
+                                    }
+                                }
+                            )
+                        except Exception as e:
+                            Error(f"Error sending message to client {str(client)}: " + str(e))
+                    else:
+                        Info(
+                            f"Client {str(client.username)} sent message: Post: {str(message["val"]["val"]["p"])}, mode: {str(message["val"]["val"]["type"])}, timestamp: {float(time.time())}"
+                        )
+                        uid = str(uuid.uuid4())
+                        db.insert_data(
+                            "posts",
+                            (
+                                str(client.username),
+                                float(time.time()),
+                                uid,
+                                str(message["val"]["val"]["p"]),
+                                False,
+                                "home",
+                                str(message["val"]["val"]["type"]),
+                            ),
+                        )
+                        await server.send_packet_multicast(
+                            server.clients_manager.clients,
+                            {
+                                "cmd": "gmsg",
                                 "val": {
-                                    "author": client.username,
-                                    "post_content": str(message["val"]["val"]["p"]),
-                                    "uid": uid,
+                                    "cmd": "rpost",
+                                    "val": {
+                                        "author": client.username,
+                                        "post_content": str(message["val"]["val"]["p"]),
+                                        "uid": uid,
+                                    },
                                 },
                             },
-                        },
-                    )
+                        )
                 case "delete":
-                    Info(
-                        f"Client {str(client.id)} sent message: UID: {str(message["val"]["val"]["uid"])}, mode: {str(message["val"]["val"]["type"])}, timestamp: {float(time.time())}"
-                    )
-                    db.update_data(
-                        "posts",
-                        {"isDeleted": True},
-                        {"uid": str(message["val"]["val"]["uid"])},
-                    )
-                    server.send_packet_multicast(
-                        server.clients_manager.clients,
+                    if (client.id not in authenticated_clients):
+                        try:
+                            await server.send_packet_unicast(
+                                client,
+                                {
+                                    "cmd": "gmsg",
+                                    "val": {
+                                        "cmd": "status",
+                                        "val": {
+                                            "message": "Not authenticated",
+                                            "username": client.username
+                                        }
+                                    }
+                                }
+                            )
+                        except Exception as e:
+                            Error(f"Error sending message to client {str(client)}: " + str(e))
+                    else:
+                        Info(
+                            f"Client {str(client.id)} sent message: UID: {str(message["val"]["val"]["uid"])}, mode: {str(message["val"]["val"]["type"])}, timestamp: {float(time.time())}"
+                        )
+                        db.update_data(
+                            "posts",
+                            {"isDeleted": True},
+                            {"uid": str(message["val"]["val"]["uid"])},
+                        )
+                        await server.send_packet_multicast(
+                            server.clients_manager.clients,
+                            {
+                                "cmd": "gmsg",
+                                "val": {
+                                    "cmd": "rdel",
+                                    "val": {"uid": str(message["val"]["val"]["uid"])},
+                                },
+                            },
+                        )
+        case "auth":
+            USER = message["val"]["val"]["username"]
+            PASSWORD = message["val"]["val"]["pswd"]
+
+            selection = db.select_data(
+                "users", {
+                    "username": USER
+                }
+            )
+            print(str(selection))
+            if selection:
+                if Fernet(KEY).decrypt(str(selection[0][9])) == PASSWORD:
+                    Info(f"Client {str(client.username)} logged in")
+                    token = jwt.encode(
                         {
-                            "cmd": "gmsg",
+                            "username": USER,
+                            "password": selection[0][9]
+                        },
+                        KEY,
+                        algorithm="HS256",
+                    )
+                    await server.send_packet_unicast(
+                        client,
+                        {
+                            "cmd": "auth",
                             "val": {
-                                "cmd": "rdel",
-                                "val": {"uid": str(message["val"]["val"]["uid"])},
+                                "token": token,
+                                "username": USER,
                             },
                         },
                     )
+                    authenticated_clients.append(client.id)
+                else:
+                    await server.send_packet_unicast(
+                        client,
+                        {
+                            "cmd": "status",
+                            "val": {
+                                "message": "Invalid password",
+                                "username": USER,
+                            },
+                        },
+                    )
+            else:
+                await server.send_packet_unicast(
+                    client,
+                    {
+                        "cmd": "status",
+                        "val": {
+                            "message": "User doesn't exist",
+                            "username": USER,
+                        }
+                    }
+                )
 
 
 @server.on_message
