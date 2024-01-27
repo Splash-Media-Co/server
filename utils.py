11583b11bfd05662a15eb9a8e92c9cff95bebcc2
GitHub <noreply@github.com>
2024-01-27 12:26:00 +0000

    Run Ruff Linter & Formatter
    Signed-off-by: GitHub <noreply@github.com>

--- utils.py
+++ utils.py
@@ -30,5 +30,6 @@ class WebSocketRateLimiter:
         else:
             return False
 
+
 async def is_client_authenticated(client_id, authenticated_clients):
     return client_id in authenticated_clients
